c63aadd556a4ab8ad981515c8b680be2dea7e458
Joao-Gabriel-Santos <jgabrielsantos280@gmail.com>
2023-05-26 15:40:02 -0300

    Update comand.js

--- comand.js
+++ comand.js
@@ -5,20 +5,20 @@ var img = window.document.getElementById('img')
 //Recebendo o horario
 var a = new Date()
 var hora = a.getHours()
+var minuto = a.getMinutes()
+
+p.innerHTML = 'Agora são ' + hora + ":" + minuto + ' horas!!!'
 
 if (hora >= 6 && hora <= 11) {
     document.body.style.background = 'rgb(251, 228, 100)'
-    p.innerHTML = 'Agora são ' + hora + " horas!!!"
     img.style.backgroundImage = 'url(img/manha.jpg)'
 }
 else if (hora > 11 && hora < 18) {
     document.body.style.background = 'rgb(116, 43, 16)'
-    p.innerHTML = 'Agora são ' + hora + " horas!!!"
     img.style.backgroundImage = 'url(img/tarde.jpg)'
 }
 else {
     document.body.style.background = 'gray'
-    p.innerHTML = 'Agora são ' + hora + " horas!!!"
     img.style.backgroundImage = 'url(img/noite.jpg)'
 }
 }
